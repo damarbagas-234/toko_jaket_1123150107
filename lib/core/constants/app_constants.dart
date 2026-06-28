e7dc89bd5f2f428a19ca8b8bc895d492a28f1f85
damarbagas-234 <muhammaddamar107@gmail.com>
2026-06-28 19:08:53 +0700

    menambahkan endpoint cart dan order

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,11 +1,21 @@
 class AppConstants {
-  static const String baseUrl = 'http://localhost:8080/v1';
+  static const String baseUrl = 'http://172.17.203.167:8080/v1';
+  // static const String baseUrl = 'http://192.168.100.7:8080/v1';
+  // static const String baseUrl = 'http://103.94.133.254:8080/v1';
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
+  static const String refreshToken = '/auth/refresh';
 
   // Product endpoints
   static const String products = '/products';
+
+  // Cart endpoints
+  static const String cart = '/cart';
+
+  // Order endpoints
+  static const String orders = '/orders';
+  static const String checkout = '/orders/checkout';
 
   // Timeout
   static const int connectTimeout = 15000;
